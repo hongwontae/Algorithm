8cd96d32dd54186ff3fa0697ae59519b66c4c88d
dnjsxoghd <feelchok1234@gmail.com>
2024-05-15 10:48:13 +0900

    Array abd Object Performance check

---
+++ Algorithm-Code/2-Array-Object-Performance/AOP.js
@@ -0,0 +1,16 @@
+let b = {
+    team : 'river',
+    year : 2020
+};
+
+const bKey = Object.keys(b);
+console.log(bKey)
+
+
+let a = [1,2,3,4,5];
+// 앞에 추가하는 것은 n/ 뒤에 추가하는 것은 상수
+
+a.push(10);
+a.shift(100); // n
+console.log(a);
+
